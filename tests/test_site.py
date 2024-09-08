66fac225dc21ee2e1e47d8c0df230f097fabd6b0
Maxim <maksim.sizov.2@gmail.com>
2024-09-08 16:24:59 +0500

    удалил комментарии

--- tests/test_site.py
+++ tests/test_site.py
@@ -15,27 +15,5 @@ def test_login(driver):
     feedpage = FeedPage(driver)
     feedpage.login_validation()
 
-    # driver.get("https://vk.com/")
-    # wait = WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.XPATH, '//input[@class="VkIdForm__input"]'))
-    # ).send_keys(phone_number)
-    # WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, "//span[text()='Войти']"))
-    # ).click()
-    # WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, '//span[text()="Подтвердить другим способом"]'))
-    # ).click()
-    # WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, '//span[text()="Пароль"]'))
-    # ).click()
-    # WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.XPATH, '//input[@type="password"]'))
-    # ).send_keys(password)
-    # WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, '//span[text()="Продолжить"]'))
-    # ).click()
-    # validation = WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Моя страница"]')))
-    # assert validation is not None
-
 
 
